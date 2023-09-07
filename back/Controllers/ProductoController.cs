a094b0c4c3c71683fc445313e1f2f90fc64eba0d
danielgil <danistcruz@gmail.com>
2023-09-06 22:20:30 -0500

    se crea los modelos request para agregar y actualizar un producto, se terminar de crear las api para producto

--- back/Controllers/ProductoController.cs
+++ back/Controllers/ProductoController.cs
@@ -1,4 +1,5 @@
 ﻿using back.Data;
+using back.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,6 +22,66 @@ namespace back.Controllers
         public async Task<IActionResult> GetProducts()
         {
             return Ok(dbContext.Products.ToListAsync());
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(AddProductoRequest addProductoRequest)
+        {
+            var product = new Producto()
+            {
+                id = Guid.NewGuid(),
+                codigo = addProductoRequest.codigo,
+                descripcion = addProductoRequest.descripcion,
+                listaDePrecios = addProductoRequest.listaDePrecios,
+                imagen = addProductoRequest.imagen,
+                productoParaLaVenta = addProductoRequest.productoParaLaVenta,
+                porcentajeIva = addProductoRequest.porcentajeIva
+            };
+
+            await dbContext.Products.AddAsync(product);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateProduct([FromRoute] Guid id, UpdateProductoRequest updateProductoRequest)
+        {
+            var product = await dbContext.Products.FindAsync(id);
+
+            if (product != null)
+            {
+
+                product.codigo = updateProductoRequest.codigo;
+                product.descripcion = updateProductoRequest.descripcion;
+                product.listaDePrecios = updateProductoRequest.listaDePrecios;
+                product.imagen = updateProductoRequest.imagen;
+                product.productoParaLaVenta = updateProductoRequest.productoParaLaVenta;
+                product.porcentajeIva = updateProductoRequest.porcentajeIva;
+
+                dbContext.SaveChangesAsync();
+
+                return Ok(product);
+            }
+            return NotFound();
+
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteProduct([FromRoute] Guid id)
+        {
+            var product = await dbContext.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                dbContext.Remove(product);
+                await dbContext.SaveChangesAsync();
+                return Ok(product);
+            }
+            return NotFound();
         }
 
     }
---
+++ back/Model/UpdateProductoRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace back.Model
+{
+    public class UpdateProductoRequest
+    {
+        public Int16 codigo { get; set; }
+        public String descripcion { get; set; }
+        public int listaDePrecios { get; set; }
+        public string imagen { get; set; }
+        public Boolean productoParaLaVenta { get; set; }
+        public Int16 porcentajeIva { get; set; }
+
+    }
+}
